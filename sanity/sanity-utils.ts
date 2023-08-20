f6b168ae5eb8e8fa4ef0dcb98d925eb6cb588d91
John Correa <jecl2912@gmail.com>
2023-08-19 23:29:32 -0500

    fix: cdn

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -8,7 +8,7 @@ export async function getProjects(): Promise <Project[]> {
             projectId: "of9c98lx",
             dataset: "production",
             apiVersion: "2023-03-04",
-            useCdn: false
+            useCdn: true
         }
     )
 
@@ -30,7 +30,7 @@ export async function getProject(slug: string): Promise <Project> {
             projectId: "of9c98lx",
             dataset: "production",
             apiVersion: "2023-03-04",            
-            useCdn: false
+            useCdn: true
         }
     )
 
